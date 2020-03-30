a93c17adab9f24f5df7a412a2d7c3f7c6dfbe889
LmMuyu <1538993458@qq.com>
2020-03-30 19:13:40 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import store from './store'
+import router from './router'
+import './plugins/vant.js'
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  store,
+  router,
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
